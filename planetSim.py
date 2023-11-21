cca02ae0b855581dc127fa8e1e820e0aa682623e
Génevieve Chikwanha <genevievechikwanha@Genevieves-MacBook-Pro.local>
2023-11-21 14:33:43 +0200

    rest of the code

--- planetSim.py
+++ planetSim.py
@@ -13,6 +13,8 @@ RED = (188, 39, 50)
 YELLOW_BROWN = (204, 153, 102)
 GRAY = (128, 128, 128)
 
+FONT = pygame.font.SysFont("Helvetica", 16)
+
 
 class Planet:
     ASTRONOMICAL_UNIT = 149.6e6 * 1000
@@ -39,6 +41,54 @@ class Planet:
         y = self.y * self.SCALE + HEIGHT / 2
         pygame.draw.circle(win, self.colour, (x, y), self.radius)
 
+        if len(self.orbit) > 2:
+            updated_points = []
+            for point in self.orbit:
+                x, y = point
+                x = x * self.SCALE + WIDTH / 2
+                y = y * self.SCALE + HEIGHT / 2
+                updated_points.append((x, y))
+
+            pygame.draw.lines(win, self.colour, False, updated_points, 2)
+
+        pygame.draw.circle(win, self.colour, (x, y), self.radius)
+
+        if not self.sun:
+            distance_text = FONT.render(f"{round(self.distance_to_sun/1000, 0)}km", 1, WHITE)
+            win.blit(distance_text, (x - distance_text.get_width()/2, y - distance_text.get_width()/2))
+
+    def attraction(self, other):
+        other_x, other_y = other.x, other.y
+        distance_x = other_x - self.x
+        distance_y = other_y - self.y
+        distance = math.sqrt(distance_x**2 + distance_y**2)
+
+        if other.sun:
+            self.distance_to_sun = distance
+
+        force = self.GRAVITATIONAL_UNIT * self.mass * other.mass / distance**2  # Gravitational force formula
+        theta = math.atan2(distance_y, distance_x)
+        force_x = math.cos(theta) * force
+        force_y = math.sin(theta) * force
+        return force_x, force_y
+
+    def update_position(self, planets):
+        total_fx = total_fy = 0
+        for planet in planets:
+            if self == planet:
+                continue
+
+            fx, fy = self.attraction(planet)
+            total_fx += fx
+            total_fy += fy
+
+        self.x_vel += total_fx / self.mass * self.TIMESTEP
+        self.y_vel += total_fy / self.mass * self.TIMESTEP
+
+        self.x += self.x_vel * self.TIMESTEP
+        self.y += self.y_vel * self.TIMESTEP
+        self.orbit.append((self.x, self.y))
+
 
 def main():
     run = True
@@ -50,22 +100,29 @@ def main():
 
     # Perhaps create an actual earth coloured earth
     earth = Planet(-1 * Planet.ASTRONOMICAL_UNIT, 0, 16, BLUE, 5.9742 * 10**24)
+    earth.y_vel = 29.783 * 1000
 
     mars = Planet(-1.524 * Planet.ASTRONOMICAL_UNIT, 0, 12, RED, 6.39 * 10**23)
+    mars.y_vel = 24.077 * 1000
 
     mercury = Planet(0.387 * Planet.ASTRONOMICAL_UNIT, 0, 8, GRAY, 3.30 * 10**23)
+    mercury.y_vel = -47.4 * 1000
 
     venus = Planet(0.723 * Planet.ASTRONOMICAL_UNIT, 0, 14, YELLOW_BROWN, 4.8685 * 10**24)
+    venus.y_vel = -35.02 * 1000
 
     planets = [sun, earth, mars, mercury, venus]
 
     while run:
         clock.tick(60)
+        WINDOW.fill((0, 0, 0))
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 run = False
 
         for planet in planets:
+            planet.update_position(planets)
             planet.draw(WINDOW)
         pygame.display.update()
 
