c4ba63087165a5e3369475fb0f93ed77df684b7c
Génevieve Chikwanha <genevievechikwanha@Genevieves-MacBook-Pro.local>
2023-11-21 11:14:22 +0200

    Creating window

--- main.py
+++ main.py
@@ -1,2 +1,2 @@
-# My new project
-#yeyeyye
+
+
---
+++ planetSim.py
@@ -0,0 +1,10 @@
+import pygame
+import math
+pygame.init()
+
+WIDTH, HEIGHT = 800, 800
+WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Planet Simulation")
+
+
+
